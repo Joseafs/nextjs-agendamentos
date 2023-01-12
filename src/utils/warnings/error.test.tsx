8585717a7f13b48f0cb79ddd4ca2a08f27eb1f41
Joseafs <i.joseafs@gmail.com>
2023-01-12 09:59:13 -0300

    fix: Teste unit. Warnings/error

---
+++ src/utils/warnings/error.test.tsx
@@ -0,0 +1,18 @@
+import { render } from '@testing-library/react';
+import {
+  errorSchedulingConflicts,
+  errorSchedulingEndBiggerThanStart
+} from './error';
+
+describe('Warnings Error', () => {
+  it('Should match text and count of errorSchedulingConflicts', () => {
+    const { container } = render(errorSchedulingConflicts(5));
+
+    expect(container).toMatchSnapshot();
+    expect(container).toHaveTextContent('Conflito(s) Identificado(s): 5');
+  });
+  it('Should match text of errorSchedulingEndBiggerThanStart', () => {
+    const { container } = render(errorSchedulingEndBiggerThanStart());
+    expect(container).toMatchSnapshot();
+  });
+});
